4f96e6d1510f4afb64b1d37994084b5fdb5dee35
Guilherme Devólio <guinogaroto@gmail.com>
2021-10-28 17:17:03 -0300

    Add Description to this file

--- App/Config/validations_errors.php
+++ App/Config/validations_errors.php
@@ -1,8 +1,12 @@
 <?php
 
+// Validation Messages
+// Return validation error messages for LunaPHP\Validator
+
 return $validations = [
     'email' => ['error_message' => 'O email informado não é valido'],
     'int' => ['error_message' => 'O valor informado precisa ser um valor do tipo inteiro'],
-    'required' => ['error_message' => 'O campo informado precisa estar preenchido']
+    'required' => ['error_message' => 'O campo informado precisa estar preenchido'],
+    'string' => ['error_message' => 'O campo informado precisa ser um valor do tipo string']
 ];
 
